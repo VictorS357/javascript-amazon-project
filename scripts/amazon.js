1fc4ab7aec74223872d054f868cc05a32a75d7b1
victor357 <victorfransousa357@gmail.com>
2026-04-21 18:32:01 -0300

    load products from backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,96 +1,100 @@
 import { cart, addToCart, calculateSiteCartNumber } from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products, loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-let productsHTML = '';
+loadProducts(renderProductsGrid);
 
-calculateSiteCartNumber();
+function renderProductsGrid() {
+  let productsHTML = '';
 
-products.forEach((product) => {
-  productsHTML += `
-    <div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image"
-          src="${product.image}">
-      </div>
+  calculateSiteCartNumber();
 
-      <div class="product-name limit-text-to-2-lines">
-        ${product.name}
-      </div>
-
-      <div class="product-rating-container">
-        <img class="product-rating-stars"
-          ${product.getStarsUrl()}
-        <div class="product-rating-count link-primary">
-          ${product.rating.count}
+  products.forEach((product) => {
+    productsHTML += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image"
+            src="${product.image}">
         </div>
+
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+          <img class="product-rating-stars"
+            ${product.getStarsUrl()}
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
+
+        <div class="product-price">
+          ${product.getPrice()}
+        </div>
+
+        <div class="product-quantity-container">
+          <select class="js-quantity-selector-${product.id}">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        ${product.extraInfoHTML()}
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart js-added-display-${product.id}">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary 
+        js-add-to-cart-btn" data-product-id="${product.id}">
+          Add to Cart
+        </button>
       </div>
-
-      <div class="product-price">
-        ${product.getPrice()}
-      </div>
-
-      <div class="product-quantity-container">
-        <select class="js-quantity-selector-${product.id}">
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-
-      ${product.extraInfoHTML()}
-
-      <div class="product-spacer"></div>
-
-      <div class="added-to-cart js-added-display-${product.id}">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
-
-      <button class="add-to-cart-button button-primary 
-      js-add-to-cart-btn" data-product-id="${product.id}">
-        Add to Cart
-      </button>
-    </div>
-  `;
+    `;
 
 
-});
-  
-document.querySelector('.js-products-grid')
-    .innerHTML = productsHTML;
-
-function addMessageDisplay(productId) {
-  const addedDisplayElement = document.querySelector(`.js-added-display-${productId}`);
-  let addedMsg = addedDisplayElement.innerHTML;
-  addedMsg = 'Added';
-  addedDisplayElement.classList.add('added-class');
-
-  const previousTimeoutId = addedMessageTimeouts[productId];
-  if (previousTimeoutId) {
-    clearTimeout(previousTimeoutId);
-  }
-  const timeoutId = setTimeout(() => {
-    addedDisplayElement.classList.remove('added-class');
-  }, 2000);
-
-  addedMessageTimeouts[productId] = timeoutId;
-}
-
-const addedMessageTimeouts = {};
-document.querySelectorAll('.js-add-to-cart-btn')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const { productId } = button.dataset;
-      addToCart(productId);
-      calculateSiteCartNumber();
-      addMessageDisplay(productId);
-    });
   });
+    
+  document.querySelector('.js-products-grid')
+      .innerHTML = productsHTML;
+
+  function addMessageDisplay(productId) {
+    const addedDisplayElement = document.querySelector(`.js-added-display-${productId}`);
+    let addedMsg = addedDisplayElement.innerHTML;
+    addedMsg = 'Added';
+    addedDisplayElement.classList.add('added-class');
+
+    const previousTimeoutId = addedMessageTimeouts[productId];
+    if (previousTimeoutId) {
+      clearTimeout(previousTimeoutId);
+    }
+    const timeoutId = setTimeout(() => {
+      addedDisplayElement.classList.remove('added-class');
+    }, 2000);
+
+    addedMessageTimeouts[productId] = timeoutId;
+  }
+
+  const addedMessageTimeouts = {};
+  document.querySelectorAll('.js-add-to-cart-btn')
+    .forEach((button) => {
+      button.addEventListener('click', () => {
+        const { productId } = button.dataset;
+        addToCart(productId);
+        calculateSiteCartNumber();
+        addMessageDisplay(productId);
+      });
+    });
+}
