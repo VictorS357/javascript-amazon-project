f1e060afcac0192b027aa092d37f14770a16f391
victor357 <victorfransousa357@gmail.com>
2026-04-25 12:01:02 -0300

    make progress bar interactive

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -22,6 +22,10 @@ async function loadPage() {
       productDetails = details;
     }
   });
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+  const deliveryProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
   
   trackHTML += `
     <a class="back-to-orders-link link-primary" href="orders.html">
@@ -42,20 +46,23 @@ async function loadPage() {
 
     <img class="product-image" src="${product.image}">
 
-    <div class="progress-labels-container">
+    <div class="progress-labels-container
+      ${deliveryProgress < 50 ? 'current-status' : ''}">
       <div class="progress-label">
         Preparing
       </div>
-      <div class="progress-label current-status">
+      <div class="progress-label 
+        ${deliveryProgress >= 50 && deliveryProgress < 100 ? 'current-status' : ''}">
         Shipped
       </div>
-      <div class="progress-label">
+      <div class="progress-label
+        ${deliveryProgress >= 100 ? 'current-status' : ''}">
         Delivered
       </div>
     </div>
 
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+      <div class="progress-bar" style="width: ${deliveryProgress}%"></div>
     </div>
   `;
 
